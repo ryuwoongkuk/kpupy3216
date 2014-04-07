91321fa4938f2de5675d7df56afff31701eb36ef
류웅국 <lyuung-gug@lyuung-gug-ui-MacBook-Air.local>
2014-04-07 17:16:12 +0900

    21

--- test13.py
+++ test13.py
@@ -7,7 +7,6 @@
 from sys import argv
 
 script, first, second, third = argv
-third=333
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
